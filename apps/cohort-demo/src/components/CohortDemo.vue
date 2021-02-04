aed0894f62a2c052295dc2eddb6959eec7bc14c1
connoratrug <c.stroomberg@rug.nl>
2021-02-04 09:13:33 +0100

    chore(cohort-demo): add some basic styling to result

--- apps/cohort-demo/src/components/CohortDemo.vue
+++ apps/cohort-demo/src/components/CohortDemo.vue
@@ -1,11 +1,31 @@
 <template>
-  <div class="hello">
+  <div class="container">
     <h1>{{ msg }}</h1>
-    <p>
+    <p class="lead">
       A demo app to take emx-2 for a spin
     </p>
     <hr>
-    <div>{{myData}}</div>
+    <table class="table caption-top">
+      <caption>Variables</caption>
+      <thead style="position: sticky; top: 20px" >
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Label</th>
+          <th scope="col">Unit</th>
+          <th scope="col">mandatory</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="variable in variableDataPage.Variables" :key="variable.name">
+          <td>{{variable.name}}</td>
+          <td>{{variable.description}}</td>
+          <td>{{variable.label}}</td>
+          <td>{{variable.unit ? variable.unit.name : ''}}</td>
+          <td>{{variable.mandatory}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -20,42 +40,26 @@ export default defineComponent({
   },
   data () {
     return {
-      myData: {}
+      variableDataPage: {}
     }
   },
   async created () {
     const query = gql`
     {
-      Variables(limit: 10) {
+      Variables(limit: 100) {
         name
         description
         label
+        mandatory
         unit{
           name
         }
       }
     }
     `
-    
-    this.myData = await request('/CohortsCentral/catalogue/graphql', query)
+
+    this.variableDataPage = await request('/CohortsCentral/catalogue/graphql', query)
   }
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
